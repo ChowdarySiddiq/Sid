22e77754b801e48f32eb4aa61297792dd2363546
ChowdarySiddiq <81869728+ChowdarySiddiq@users.noreply.github.com>
2021-04-03 21:19:10 +0530

    Add files via upload
    A Responsive UPI Keyboard.

---
+++ keyboard.js
@@ -0,0 +1,332 @@
+$(document).ready(function(){
+	$("#num1").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "1");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#num2").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "2");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+
+	})
+	$("#num3").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "3");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#num4").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "4");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#num5").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "5");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#num6").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "6");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#num7").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "7");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#num8").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "8");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#num9").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "9");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#numdel").click(function(){
+		var Existing_Number = $("#field").val();
+		var new_number = Existing_Number.slice(0, -1);
+		$("#field").val(new_number);
+		
+		var Checking = $(".Eye").attr("fa-eye");
+		
+		if(new_number.length == 1)
+		{
+			$("#star").text("*");
+			$("#stars").text("");
+			$("#starss").text("");
+			$("#starsss").text("");
+		}
+		else
+		{
+			$("#star").text("");
+		}
+		if(new_number.length == 2)
+		{
+			$("#star").text("*");
+			$("#stars").text("*");
+			$("#starss").text("");
+			$("#starsss").text("");
+		}
+		else
+		{
+			$("#stars").text("");
+		}
+		if(new_number.length == 3)
+		{
+			$("#star").text("*");
+			$("#stars").text("*");
+			$("#starss").text("*");
+			$("#starsss").text("");
+		}
+		else
+		{
+			$("#starss").text("");
+		}
+
+		if(new_number.length == 4)
+		{
+			$("#star").text("*");
+			$("#stars").text("*");
+			$("#starss").text("*");
+			$("#starsss").text("*");
+		}
+		else
+		{
+			$("#starsss").text("");
+		}
+		$("#Incor").text("");
+	})
+	$("#num0").click(function(){
+		var Existing_Number = $("#field").val();
+		$("#field").val(Existing_Number + "0");
+		if(Existing_Number.length == 0)
+		{
+			$("#star").text("*");	
+		}
+		if(Existing_Number.length == 1)
+		{
+			$("#stars").text("*");
+		}
+		if(Existing_Number.length == 2)
+		{
+			$("#starss").text("*");
+		}
+		if(Existing_Number.length == 3)
+		{
+			$("#starsss").text("*");
+		}
+		if(Existing_Number.length >=  4)
+		{
+			$("#field").val(Existing_Number[0] + Existing_Number[1] + Existing_Number[2] + Existing_Number[3])
+		}
+	})
+	$("#numnext").click(function(){
+		var Sid = 1602;
+		var UserPass = $("#field").val();
+		if(Sid == UserPass)
+		{
+			alert("password correct");
+		}
+		else
+		{
+			$(UserPass).val("");
+			$(star).text("");
+			$(stars).text("");
+			$(starss).text("");
+			$(starsss).text("");
+			$("#Incor").html("Incorrect Password");
+		}
+	})
+	$(".Eye").click(function(){
+		
+		var Eyes = $("#field").val();
+		var Eyed = $(".Eye").attr("class");
+		alert(Eyed);
+		if(Eyes.length > 0 )
+		{
+		$(".Eye").removeClass("fa-eye-slash");
+		$(".Eye").addClass("fa-eye");
+		$(".Eye").css("opacity", "1");
+		$("#star").text(Eyes[0]);
+		$("#stars").text(Eyes[1]);
+		$("#starss").text(Eyes[2]);
+		$("#starsss").text(Eyes[3]);
+		}
+		
+	})
+	
+})
